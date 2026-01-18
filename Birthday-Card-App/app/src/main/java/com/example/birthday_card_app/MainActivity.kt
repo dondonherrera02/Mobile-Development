28406026b26d33488bc9a98586068ef6c55a0a9c
dondonherrera <dondonherrera02@gmail.com>
2026-01-18 16:32:13 -0700

    Added Bday Card App

--- Birthday-Card-App/app/src/main/java/com/example/birthday_card_app/MainActivity.kt
+++ Birthday-Card-App/app/src/main/java/com/example/birthday_card_app/MainActivity.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -16,12 +14,17 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.birthday_card_app.ui.theme.BirthdayCardAppTheme
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.ui.layout.ContentScale
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,12 +34,15 @@ class MainActivity : ComponentActivity() {
             BirthdayCardAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Surface(
-                        modifier = Modifier.fillMaxSize(),
-                        color = MaterialTheme.colorScheme.background
+                        modifier =
+                            Modifier
+                                .fillMaxSize()
+                                .padding(innerPadding),
+                    color = MaterialTheme.colorScheme.background
                     ) {
                         GreetingText(
                             message = stringResource(R.string.hbd_text),
-                            from = "From Dondon H.",
+                            from = stringResource(R.string.from_dondon_h),
                             modifier = Modifier.padding(8.dp)
                         )
                     }
@@ -58,35 +64,60 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
     Column(
         verticalArrangement = Arrangement.Center,
-        modifier = modifier.padding(8.dp)
+        modifier = modifier
+            .fillMaxSize()
+            .padding(8.dp)
     ) {
-        Row {
-            Text(
-                text = message,
-                fontSize = 95.sp,
-                lineHeight = 116.sp,
-                textAlign = TextAlign.Center
-            )
-        }
-        Row {
-            Text(
-                text = from,
-                fontSize = 36.sp,
-                textAlign = TextAlign.End,
-                modifier = modifier
-                    .fillMaxWidth()
-                    .padding(16.dp)
+        Text(
+            text = message,
+            fontSize = 95.sp,
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxWidth()
+        )
 
-            )
-        }
+        Text(
+            text = from,
+            fontSize = 36.sp,
+            textAlign = TextAlign.End,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        )
     }
 }
+
+@Composable
+fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.androidparty)
+
+    Box(modifier.fillMaxSize()) {
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            alpha = 0.5f,
+            modifier = Modifier.fillMaxSize()
+        )
+
+        GreetingText(
+            message = message,
+            from = from
+        )
+    }
+}
+
+
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     BirthdayCardAppTheme {
         //Greeting("Android")
-        GreetingText(message = stringResource(R.string.hbd_text), from = "Dondon")
+        GreetingImage(
+            message = stringResource(R.string.hbd_text),
+            from = stringResource(R.string.from_dondon_h)
+        )
     }
 }
